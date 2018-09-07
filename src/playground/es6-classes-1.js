675b6a7bfa4d899ab1389d436fad53135d9da47f
Rob Marshall <rob@fox.agency>
2018-09-07 16:52:01 +0100

    Added person class

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name = 'Anon') {
+        this.name = name
+    }
+    getGreeting() {
+        return 'Hi. I am ' + this.name + '!';
+    }
+}
+
+const me = new Person();
+
+console.log(me.getGreeting());
