1689c0f0234edebffa06b24ac2b06b1a9d3eecb1
Rob Marshall <rob@fox.agency>
2018-09-07 17:21:19 +0100

    Added more to class

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -7,17 +7,29 @@ class Person {
         return `Hi. I am ${ this.name }!`;
     }
     getDescription() {
-        return `${ this.name } is ${ this.age } year(s) old`;
+        return `${ this.name } is ${ this.age } year(s) old.`;
     }
 }
 
 class Student extends Person {
     constructor(name, age, major){
-        super(name, age); // This calls the first constructor first
+        super(name, age); // This calls the parent constructor first
         this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if( this.hasMajor() ){
+            description += ` Their major is ${this.major}`;
+        }
+
+        return description;
     }
 }
 
 const me = new Student('Rob', 25, 'Computing');
 
-console.log(me);
+console.log(me.getDescription());
